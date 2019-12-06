a3e4355ec8bfc56d976e57f110368ae46741d824
thuan.dh <thuan14121999@gmail.com>
2019-12-06 23:31:57 +0700

    edit shell.sh

--- shell.sh
+++ shell.sh
@@ -1,3 +1,15 @@
-rm a.out
+file="./a.out"
+if [ -f "$file" ]
+then 
+    rm "$file"
+fi
 gcc main.c jval.o dllist.o jrb.o
-./a.out
+
+file="./a.out"
+if [ -f "$file" ]
+then 
+    echo "------------------------------------------"
+    echo "Complie success."
+    echo "Run result."
+    ./a.out
+fi
