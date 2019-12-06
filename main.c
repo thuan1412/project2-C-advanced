fe1b90327c31c257834d13bdb4a8916bfa7d4b37
thuan.dh <thuan14121999@gmail.com>
2019-12-06 21:32:33 +0700

    funtionalize txt2graph

--- main.c
+++ main.c
@@ -28,14 +28,15 @@ typedef struct {
 Graph createGraph();
 void addVertex(Graph g,  char *station_name, char *bus_list);
 char *getVertex(Graph graph, char *station_name);
-void addEdge(Graph graph, int v1, int v2, double weight);
-double getEdgeValue(Graph graph, int v1, int v2);
+void addEdge(Graph g, char *station1, char *station2, double distance);
+double getEdgeValue(Graph graph, char *station1, char *station2);
 int indegree(Graph graph, int v, int* output);
 int outdegree(Graph graph, int v, int* output);
 void dropGraph(Graph graph);
 double shortestPath(Graph graph, int s, int t, int* path, int*length);
 int compareBusStation(Jval j1, Jval j2);
 int updateVertex(Graph g, char *station_name, char *new_bus);
+Graph dataToGraph(char *filename);
 
 void trim(char * str);
 
@@ -43,7 +44,7 @@ void trim(char * str);
 Bus bus_list[150];
 int bus_count = 0;
 
-char** string_split(char* str, const char delimiter) {
+char** stringSplit(char* str, const char delimiter) {
     char** result = 0;
     size_t count = 0;
     char* temp = str;
@@ -84,9 +85,34 @@ char** string_split(char* str, const char delimiter) {
 }
 
 
-
 int main() {
-    FILE *bus_data = fopen("bus_full.txt", "r");
+    // FILE *bus_data = fopen("bus_test.txt", "r");
+    Graph g = createGraph();
+
+    g = dataToGraph("bus_full.txt");
+    char *array1 = "1 2 2";
+    addVertex(g, "1", array1);
+
+    // updateVertex(g, "1", "3");
+    char * result = getVertex(g, "1");
+    if (result) {
+        printf("%s\n", result);
+    }
+    return 0;
+
+}
+
+
+Graph createGraph() {
+    Graph g;
+    g.edges = make_jrb();
+    g.vertices = make_jrb();
+    return g;
+}
+
+
+Graph dataToGraph(char *filename) {
+    FILE *bus_data = fopen(filename, "r");
     Graph g = createGraph();
 
     while (!feof(bus_data)) {
@@ -95,19 +121,16 @@ int main() {
         fgets(s, 1000, bus_data);    
         s[strlen(s)-1] = '\0';    
         char ** token = 0;
-        
         char *bus_name=0;
         bus_name = malloc(5);
 
-        token = string_split(s, ':');
+        token = stringSplit(s, ':');
         strcpy(bus_name, *(token + 0));
         char *station_lines = *(token + 1);
-        strcpy(bus_list[bus_count].bus_name, bus_name);
 
         char **bus_stations;
-        addVertex(g, *(token+0), *(token+1));
         
-        bus_stations = string_split(*(token + 1), '-');
+        bus_stations = stringSplit(*(token + 1), '-');
 
         if (bus_stations) {
             int i;
@@ -122,38 +145,8 @@ int main() {
             free(bus_stations);
         }
     }
-
-    char *array1 = "1 2 2";
-    // char *array2  = "3 4";
-    // char *array3  = "5 6";
-    // Graph g = createGraph();
-    addVertex(g, "1", array1);
-    // addVertex(g, "2", array2);
-    // addVertex(g, "3", array3);
-
-    // updateVertex(g, "1", "3");
-    char * result = getVertex(g, "Ben xe Giap Bat");
-    if (result) {
-        printf("%s\n", result);
-    }
-    return 0;
-
-}
-
-Graph createGraph() {
-    Graph g;
-    g.edges = make_jrb();
-    g.vertices = make_jrb();
     return g;
 }
-
-// int compareBusStation(Jval j1, Jval j2) {
-//     BusStation *p1, *p2;
-//     p1 = (BusStation *) j1.v;
-//     p2 = (BusStation *) j2.v;
-
-//     return strcmp(p1->station_name, p2->station_name);
-// }
 
 
 void addVertex(Graph g,  char *station_name, char *bus_list) {
@@ -161,6 +154,10 @@ void addVertex(Graph g,  char *station_name, char *bus_list) {
     if (node == NULL) {
         jrb_insert_str(g.vertices, station_name, new_jval_s(bus_list));
     }
+}
+
+void addEdge(Graph g, char *station1, char *station2, double distance) {
+
 }
 
 char *getVertex(Graph g, char *station_name) {
@@ -171,6 +168,7 @@ char *getVertex(Graph g, char *station_name) {
         return jval_s(node->val);
     }
 }
+
 
 int updateVertex(Graph g, char *station_name, char new_bus[10]) {
     JRB node = jrb_find_str(g.vertices, station_name);
@@ -189,19 +187,15 @@ int updateVertex(Graph g, char *station_name, char new_bus[10]) {
     return 0;
 }
 
+
 void trim(char * str) {
     int index, i;
-
-    /*
-     * Trim leading white spaces
-     */
     index = 0;
     while(str[index] == ' ' || str[index] == '\t' || str[index] == '\n')
     {
         index++;
     }
 
-    /* Shift all trailing characters to its left */
     i = 0;
     while(str[i + index] != '\0')
     {
@@ -210,10 +204,6 @@ void trim(char * str) {
     }
     str[i] = '\0'; // Terminate string with NULL
 
-
-    /*
-     * Trim trailing white spaces
-     */
     i = 0;
     index = -1;
     while(str[i] != '\0')
@@ -225,7 +215,5 @@ void trim(char * str) {
 
         i++;
     }
-
-    /* Mark the next character to last non white space character as NULL */
     str[index + 1] = '\0';
 }
--- shell.sh
+++ shell.sh
@@ -1,1 +1,3 @@
+rm a.out
 gcc main.c jval.o dllist.o jrb.o
+./a.out
